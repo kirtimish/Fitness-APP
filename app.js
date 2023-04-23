e56ce81fc177634a80efdbdd2eb896163a661bbc
kirtimish <kirtimishra0708@gmail.com>
2023-04-24 04:51:53 +0530

    setting up server and connection to mongoose

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose');
+ //routes
+// const productRoutes = require('./routes/product')   
+const errorController = require('./controllers/error');
+const app = express();
+
+//MIDDLEWARES 
+app.use(cors());     //For cross-origin resources sharing
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(bodyParser.urlencoded({ extended:true }))
+app.use(bodyParser.json());
+// app.use(productRoutes)
+app.use(errorController.get404);
+
+mongoose.connect('mongodb+srv://kirti:test1234@cluster0.wgasf.mongodb.net/Fitness-APP?retryWrites=true&w=majority')
+.then(result => {
+    app.listen(3000)
+    console.log('Connected!')
+})
+.catch(err => {
+    console.log(err)
+})
